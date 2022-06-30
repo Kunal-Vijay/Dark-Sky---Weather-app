e05e7d40a46b2b5995f139a1faf7672e97da9e20
Kunal <kunal20vijay00@gmail.com>
2022-06-30 20:34:36 +0530

    added topbuttons component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Topbuttons from './components/Topbuttons';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Dark Sky - Weather app</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <Topbuttons/>
     </div>
   );
 }
