b0cb35be8d3656ebc78ed4c6141d047b2d86f693
Kunal <kunal20vijay00@gmail.com>
2022-07-02 21:45:28 +0530

    activating top buttons and getting user inputs

--- src/components/Topbuttons.jsx
+++ src/components/Topbuttons.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Topbuttons() {
+export default function Topbuttons({setQuery}) {
   const cities = [
     {
         id: 1,
@@ -26,7 +26,7 @@ export default function Topbuttons() {
   return (
     <div className="flex items-center justify-around my-6">
       {cities.map((city) => (
-        <button key={city.id} className="text-white text-lg font-medium">{city.title}</button>
+        <button key={city.id} className="text-white text-lg font-medium" onClick={()=>{setQuery({q:city.title})}}>{city.title}</button>
       ))}
     </div>
   );
