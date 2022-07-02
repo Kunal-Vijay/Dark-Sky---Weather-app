7c3bd23b3a7ad47a9b18de8126f0fc7884147866
Kunal <kunal20vijay00@gmail.com>
2022-07-02 22:19:28 +0530

    added toastify alerts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import TempAndDetails from "./components/TempAndDetails";
 import TimeandLocation from "./components/TimeandLocation";
 import Topbuttons from "./components/Topbuttons";
 import getFormattedWeatherData from "./services/WeatherServices";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [query, setQuery] = useState({ q: "berlin" });
@@ -14,7 +16,12 @@ function App() {
 
   useEffect(() => {
     const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location";
+
+      toast.info("Fetching weather for " + message);
+
       await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(`Successfully fetched weather for ${data.name},${data.country}`)
         setWeather(data);
       });
     };
@@ -24,12 +31,14 @@ function App() {
   const formatBackground = () => {
     if (!weather) return "from-cyan-700 to-blue-700";
     const threshold = units === "metric" ? 20 : 60;
-    if(weather.temp<=threshold) return "from-cyan-700 to-blue-700";
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
 
     return "from-yellow-700 to-orange-700";
   };
   return (
-    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br ${formatBackground()} h-fit shadow-xl shadow-gray-400`}>
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br ${formatBackground()} h-fit shadow-xl shadow-gray-400`}
+    >
       <Topbuttons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
       {weather && (
@@ -40,6 +49,8 @@ function App() {
           <Forecast title={"daily forecast"} items={weather.daily} />
         </div>
       )}
+
+      <ToastContainer autoClose={3000} theme="colored" newestOnTop={true} />
     </div>
   );
 }
